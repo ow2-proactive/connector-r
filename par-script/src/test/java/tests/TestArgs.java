b966fd534b0dd64957765c3edd817ff2e085449a
Vladimir Bodnartchouk <Vladimir.Bodnartchouk@activeeon.com>
2014-06-10 17:24:04 +0200

    Format all source with eclipse formatter.

--- par-script/src/test/java/tests/TestArgs.java
+++ par-script/src/test/java/tests/TestArgs.java
@@ -13,28 +13,26 @@ import org.ow2.proactive.scripting.ScriptResult;
 import org.ow2.proactive.scripting.SimpleScript;
 import org.ow2.proactive.scripting.TaskScript;
 
+
 /**
- * Tests arguments of a script task.
- * 
+ * Tests arguments of a script task. 
+ *
  * @author Activeeon Team
  */
 @RunWith(JUnit4.class)
 public class TestArgs {
 
-	@Test
-	public void test() throws Exception {
-		String[] args = { "one", "two", "three" };
-		String rScript = "result = args[3];";
+    @Test
+    public void test() throws Exception {
+        String[] args = { "one", "two", "three" };
+        String rScript = "result = args[3];";
 
-		Map<String, Object> aBindings = Collections.singletonMap(
-				TaskScript.ARGUMENTS_NAME, (Object) args);
-		SimpleScript ss = new SimpleScript(rScript,
-				PARScriptFactory.ENGINE_NAME);
-		TaskScript taskScript = new TaskScript(ss);
-		ScriptResult<Serializable> res = taskScript.execute(aBindings);
+        Map<String, Object> aBindings = Collections.singletonMap(TaskScript.ARGUMENTS_NAME, (Object) args);
+        SimpleScript ss = new SimpleScript(rScript, PARScriptFactory.ENGINE_NAME);
+        TaskScript taskScript = new TaskScript(ss);
+        ScriptResult<Serializable> res = taskScript.execute(aBindings);
 
-		Assert.assertEquals(
-				"The arguments are not transfered by the engine to the script",
-				args[2], (String) res.getResult());
-	}
+        Assert.assertEquals("The arguments are not transfered by the engine to the script", args[2],
+                (String) res.getResult());
+    }
 }
