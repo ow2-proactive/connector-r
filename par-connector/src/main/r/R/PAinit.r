288a28dea4c7b3cd4d62f8f11a658741870e1a58
fviale <fabien.viale@activeeon.com>
2015-07-06 17:40:31 +0200

    improved PAinit to handle documentation creation

--- par-connector/src/main/r/R/PAinit.r
+++ par-connector/src/main/r/R/PAinit.r
@@ -22,14 +22,25 @@ local({
     require(devtools)
     wd(pkg = "PARConnector", path = "")
   }
+
+  require(rJava)
   pkg.root <- getwd()
-  print("Building PARConnector from :")
-  print(pkg.root)  
+  if ((basename(pkg.root) == "R") && (length(list.files(pattern="DESCRIPTION")) == 0)) {
+    pkg.root <- dirname(pkg.root)
+  }
+  cat("Building PARConnector from :\n")
+  print(pkg.root)
   .jinit()
   .jaddClassPath(dir(file.path(pkg.root,"inst","java"), full.names=TRUE))
+  cat("---- Classpath of PARConnector (make sure it contains all ProActive jars) : ----\n")
   print(.jclassPath())
+  cat("-------------------------------  End of class path -----------------------------\n")
+  cat("If an error occurs in the following code, then there is a problem with the classpath or jar versions\n\n")
+
+
   # if this fails, there is a problem with classloading
   J("org.ow2.proactive.scripting.Script")
+
  })
 
 .findroot <- function() {
